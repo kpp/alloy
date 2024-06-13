c9910e63fde67ed43b6e228c0098e51559bea431
mattsse <mattsse@users.noreply.github.com>
2024-06-13 17:06:46 +0000

    deploy: 4157e2664fab08941a378198293c7ad376078b5e

---
+++ trait.impl/alloy_signer/signer/trait.SignerSync.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"alloy":[],
+"alloy_signer":[],
+"alloy_signer_local":[["impl&lt;C: PrehashSigner&lt;(Signature, RecoveryId)&gt;&gt; SignerSync for <a class=\"struct\" href=\"alloy_signer_local/struct.LocalSigner.html\" title=\"struct alloy_signer_local::LocalSigner\">LocalSigner</a>&lt;C&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
